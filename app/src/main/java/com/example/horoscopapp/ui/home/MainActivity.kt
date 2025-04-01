875390c8afbe43ac3beaa9b06165a65c69659100
hector-a1 <hector11a@hotmail.com>
2025-04-01 17:53:05 -0600

    proyecto con los fragment vacios

---
+++ app/src/main/java/com/example/horoscopapp/ui/home/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.horoscopapp.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.horoscopapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopFragment.kt
@@ -0,0 +1,24 @@
+package com.example.horoscopapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscopapp.databinding.FragmentHoroscopBinding
+
+class HoroscopFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopBinding?=null
+    private val binding get()=_binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding=FragmentHoroscopBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+}
